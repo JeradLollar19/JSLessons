cbbad73ea36de1ce7e06f2abae4b77a233439a3e
Jerad Lollar <jeradlollar@gmail.com>
2020-03-17 17:21:23 -0400

    initial

---
+++ JSFundamentals/2-ExpressionsAndOperators/02-comparisonOperators.js
@@ -0,0 +1,31 @@
+// Falsy Value: undefined, null, NaN (NotaNumber), 0, '', false
+
+// Equal
+"3" == 3;
+
+// Strict Equal
+"3" === 3;
+
+// Not Equal
+"3" != 4;
+
+// Strict Not Equal
+"3" !== 3;
+
+// Greater Than
+3 > 2;
+
+// Less Than
+3 < 2;
+
+// Greater Than or Equal to
+3 >= 1;
+
+// Less Than or Equal to
+3 <= 2;
+
+// And
+2 > 1 && 2 < 5;
+
+// Or
+2 > 1 || 2 < 5;
