15c6c47cb62c1cdc5e452d302d0edc11f5ba9fb3
Jerad Lollar <jeradlollar@gmail.com>
2020-03-27 10:13:51 -0400

    initial

---
+++ JSFundamentals/5-functions/review.js
@@ -0,0 +1,13 @@
+function nameOfFunction (parameter1, parameter2) {
+    // Define the procedure 
+    return "something"
+}
+let iCaptureReturn = nameOfFunction(3,5)
+nameOfFunction('today', []);
+
+
+
+function greeting (fname, lname) {
+    console.log(fname + ' ' + lname);
+}
+greeting('Jerad', 'Lollar');
